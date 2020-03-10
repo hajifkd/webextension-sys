37c85ae9bbec8229f1f03d0bb11b050bff80dd7d
Hajime Fukuda <hajime.fukuda@me.com>
2020-03-10 14:17:31 +0900

    use heck instead

--- build.rs
+++ build.rs
@@ -1,19 +1,63 @@
-extern crate inflector;
+extern crate heck;
 extern crate tokio;
 extern crate webext_parser;
 
-use inflector::Inflector;
-use std::path::Path;
+use heck::SnakeCase;
+use std::collections::HashSet;
+use std::path::{Path, PathBuf};
 use tokio::fs;
+use tokio::prelude::*;
+
+fn get_dir() -> PathBuf {
+    Path::new("src").join("ext")
+}
+
+async fn create_subnamespace(names: &[&str]) -> Result<PathBuf, Box<dyn std::error::Error>> {
+    let mut path = get_dir();
+    for i in 0..names.len() {
+        if i == names.len() - 1 {
+            path.push(format!("{}.rs", names[i]))
+        } else {
+            let mut path_mod_dir = get_dir();
+            for j in 0..=i {
+                path_mod_dir.push(names[j]);
+            }
+
+            if !path_mod_dir.exists() {
+                fs::create_dir(&path_mod_dir).await?;
+            }
+
+            let mut path_mod_file = get_dir();
+            for j in 0..i {
+                path_mod_file.push(names[j]);
+            }
+            path_mod_file.push(format!("{}.rs", names[i]));
+
+            let mut file = fs::OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open(path_mod_file)
+                .await?;
+
+            file.write_all(format!("pub mod {};\n", names[i + 1]).as_bytes())
+                .await?;
+
+            path.push(names[i])
+        }
+    }
+
+    Ok(path)
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut namespaces = vec![];
+    let mut namespaces = HashSet::new();
 
-    let path_ext_dir = Path::new("src").join("ext");
-    if !path_ext_dir.exists() {
-        fs::create_dir(&path_ext_dir).await?;
+    let path_ext_dir = get_dir();
+    if path_ext_dir.exists() {
+        fs::remove_dir_all(&path_ext_dir).await?;
     }
+    fs::create_dir(&path_ext_dir).await?;
 
     for (space, url) in webext_parser::api_pages().await?.iter() {
         /* if space != "windows" {
@@ -21,9 +65,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         } */
         let namespace = webext_parser::parse_apis(&space, &url).await?;
         let name = namespace.name().to_snake_case();
-        let path = Path::new("src").join("ext").join(format!("{}.rs", &name));
+        let path = if name.contains('.') {
+            let names = name.split('.').collect::<Vec<_>>();
+            namespaces.insert(names[0].to_owned());
+            create_subnamespace(&names).await?
+        } else {
+            let path = get_dir().join(format!("{}.rs", &name));
+            namespaces.insert(name);
+            path
+        };
+
         fs::write(&path, "\n").await?;
-        namespaces.push(name)
     }
 
     let path_root = Path::new("src").join("ext.rs");
@@ -31,7 +83,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         &path_root,
         namespaces
             .iter()
-            .map(|s| format!("pub mod {};", s))
+            .map(|s| format!("#[cfg({})]\npub mod {};", &s, &s))
             .fold(String::new(), |acc, x| format!("{}{}\n", acc, x)),
     )
     .await?;
--- src/ext.rs
+++ src/ext.rs
@@ -1,64 +1,174 @@
-pub mod accessibility_features;
-pub mod alarms;
-pub mod bookmarks;
-pub mod browser_action;
-pub mod browsing_data;
-pub mod certificate_provider;
-pub mod commands;
-pub mod content_settings;
-pub mod context_menus;
-pub mod cookies;
-pub mod debugger;
-pub mod declarative_content;
-pub mod desktop_capture;
-pub mod devtools_inspected_window;
-pub mod devtools_network;
-pub mod devtools_panels;
-pub mod document_scan;
-pub mod downloads;
-pub mod enterprise_device_attributes;
-pub mod enterprise_hardware_platform;
-pub mod enterprise_platform_keys;
-pub mod events;
-pub mod extension;
-pub mod extension_types;
-pub mod file_browser_handler;
-pub mod file_system_provider;
-pub mod font_settings;
-pub mod gcm;
-pub mod history;
-pub mod i_1_8n;
-pub mod identity;
-pub mod idle;
-pub mod input_ime;
-pub mod instance_id;
-pub mod management;
-pub mod networking_config;
-pub mod notifications;
-pub mod omnibox;
-pub mod page_action;
-pub mod page_capture;
-pub mod permissions;
-pub mod platform_keys;
-pub mod power;
-pub mod printer_provider;
-pub mod printing_metrics;
-pub mod privacy;
-pub mod proxy;
-pub mod runtime;
-pub mod sessions;
-pub mod storage;
-pub mod system_cpu;
-pub mod system_memory;
-pub mod system_storage;
-pub mod tab_capture;
-pub mod tabs;
-pub mod top_sites;
-pub mod tts;
-pub mod tts_engine;
-pub mod types;
-pub mod vpn_provider;
-pub mod wallpaper;
-pub mod web_navigation;
+#[cfg(web_request)]
 pub mod web_request;
+
+#[cfg(bookmarks)]
+pub mod bookmarks;
+
+#[cfg(top_sites)]
+pub mod top_sites;
+
+#[cfg(printer_provider)]
+pub mod printer_provider;
+
+#[cfg(browsing_data)]
+pub mod browsing_data;
+
+#[cfg(extension)]
+pub mod extension;
+
+#[cfg(alarms)]
+pub mod alarms;
+
+#[cfg(permissions)]
+pub mod permissions;
+
+#[cfg(extension_types)]
+pub mod extension_types;
+
+#[cfg(declarative_content)]
+pub mod declarative_content;
+
+#[cfg(font_settings)]
+pub mod font_settings;
+
+#[cfg(desktop_capture)]
+pub mod desktop_capture;
+
+#[cfg(notifications)]
+pub mod notifications;
+
+#[cfg(input)]
+pub mod input;
+
+#[cfg(idle)]
+pub mod idle;
+
+#[cfg(context_menus)]
+pub mod context_menus;
+
+#[cfg(content_settings)]
+pub mod content_settings;
+
+#[cfg(history)]
+pub mod history;
+
+#[cfg(platform_keys)]
+pub mod platform_keys;
+
+#[cfg(sessions)]
+pub mod sessions;
+
+#[cfg(browser_action)]
+pub mod browser_action;
+
+#[cfg(gcm)]
+pub mod gcm;
+
+#[cfg(types)]
+pub mod types;
+
+#[cfg(runtime)]
+pub mod runtime;
+
+#[cfg(storage)]
+pub mod storage;
+
+#[cfg(debugger)]
+pub mod debugger;
+
+#[cfg(commands)]
+pub mod commands;
+
+#[cfg(downloads)]
+pub mod downloads;
+
+#[cfg(accessibility_features)]
+pub mod accessibility_features;
+
+#[cfg(management)]
+pub mod management;
+
+#[cfg(file_browser_handler)]
+pub mod file_browser_handler;
+
+#[cfg(events)]
+pub mod events;
+
+#[cfg(instance_id)]
+pub mod instance_id;
+
+#[cfg(web_navigation)]
+pub mod web_navigation;
+
+#[cfg(file_system_provider)]
+pub mod file_system_provider;
+
+#[cfg(tab_capture)]
+pub mod tab_capture;
+
+#[cfg(wallpaper)]
+pub mod wallpaper;
+
+#[cfg(windows)]
 pub mod windows;
+
+#[cfg(system)]
+pub mod system;
+
+#[cfg(page_action)]
+pub mod page_action;
+
+#[cfg(devtools)]
+pub mod devtools;
+
+#[cfg(identity)]
+pub mod identity;
+
+#[cfg(proxy)]
+pub mod proxy;
+
+#[cfg(certificate_provider)]
+pub mod certificate_provider;
+
+#[cfg(power)]
+pub mod power;
+
+#[cfg(vpn_provider)]
+pub mod vpn_provider;
+
+#[cfg(networking)]
+pub mod networking;
+
+#[cfg(omnibox)]
+pub mod omnibox;
+
+#[cfg(page_capture)]
+pub mod page_capture;
+
+#[cfg(tabs)]
+pub mod tabs;
+
+#[cfg(tts)]
+pub mod tts;
+
+#[cfg(tts_engine)]
+pub mod tts_engine;
+
+#[cfg(i18n)]
+pub mod i18n;
+
+#[cfg(document_scan)]
+pub mod document_scan;
+
+#[cfg(privacy)]
+pub mod privacy;
+
+#[cfg(printing_metrics)]
+pub mod printing_metrics;
+
+#[cfg(cookies)]
+pub mod cookies;
+
+#[cfg(enterprise)]
+pub mod enterprise;
+
